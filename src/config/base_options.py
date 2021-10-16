13cd0d40e35bec64e5ca13536fb7b1f2a16a9b4a
josearangos <jose.arangos@udea.edu.co>
2021-10-16 15:35:49 -0500

    base file structured project

---
+++ src/config/base_options.py
@@ -0,0 +1,31 @@
+import argparse
+
+
+
+class BaseOptions():
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.initialized = False
+
+    def initialize(self):  
+        self.parser.add_argument('--experiment_name', type=str, default='Train convtasnet', help='Experiment name ')
+        self.parser.add_argument('--tags', type=str, default='train_test_convtasnet', help='Tags of experiment name')
+        self.parser.add_argument('--root_csv', type=str, default='../../data/csv/', help='CSV located ')
+        self.parser.add_argument('--save_best_model', type=str, default='/content/drive/Shareddrives/TG-Separación-Fuentes/code/Checkpoints-separation-models/', help='Path save models')
+        self.parser.add_argument('--default_root_dir', type=str, default='/content/drive/Shareddrives/TG-Separación-Fuentes/code/Checkpoints-separation-models/', help='Path save models')
+        self.parser.add_argument('--train_csv', type=str, default='mixture_train_mix_clean_callfriend_spanish.csv', help='csv train model')
+        self.parser.add_argument('--valid_csv', type=str, default='mixture_val_mix_clean_callfriend_spanish.csv', help='csv valid model')
+        self.parser.add_argument('--test_csv', type=str, default='mixture_test_mix_clean_callfriend_spanish.csv', help='cvs test model')
+        self.parser.add_argument('--path_config', type=str, default='../config/conf.yml', help='path config experiment')
+        self.parser.add_argument('--batch_size', type=int, default=4, help='batch size to train')
+        self.parser.add_argument('--num_workers', type=int, default=4, help='# Workers')
+
+        
+
+        self.initialized = True
+
+    def parse(self):
+        if not self.initialized:
+            self.initialize()
+        self.opt = self.parser.parse_args()
+        return self.opt
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -24,21 +24,26 @@ from asteroid.engine.optimizers import make_optimizer
 from pytorch_lightning.loggers.neptune import NeptuneLogger
 from ..data.CallSpanish_dataset import CallSpanish
 import pandas as pd
+from ..config.base_options import BaseOptions
 
 class Train:
-    def __init__(self,save_best_model,default_root_dir,ROOT_CSV = "../../data/csv/",train_csv="mixture_train_mix_clean_callfriend_spanish.csv",valid_csv="mixture_val_mix_clean_callfriend_spanish.csv",test_csv = "mixture_test_mix_clean_callfriend_spanish.csv"):
-        self.ROOT_CSV = ROOT_CSV
-        self.PATH_CSV_TRAIN = self.ROOT_CSV + train_csv 
-        self.PATH_CSV_VALID = self.ROOT_CSV + valid_csv
-        self.PATH_CSV_TEST = self.ROOT_CSV + test_csv
-        self.PATH_CONFIG = "../config/conf.yml"
+    def __init__(self,opt):
+        
+        self.opt =opt
+        self.experiment_name = opt.experiment_name
+        self.tags= opt.tags
+        self.ROOT_CSV = opt.root_csv
+        self.PATH_CSV_TRAIN = self.root_csv + opt.train_csv 
+        self.PATH_CSV_VALID = self.root_csv + opt.valid_csv
+        self.PATH_CSV_TEST = self.root_csv + opt.test_csv
+        self.PATH_CONFIG = opt.PATH_CONFIG
         self.df_train = pd.read_csv(self.PATH_CSV_TRAIN)
         self.df_val = pd.read_csv(self.PATH_CSV_VALID)
         self.df_test = pd.read_csv(self.PATH_CSV_TEST)
-        self.save_best_model = save_best_model
-        self.default_root_dir = default_root_dir
+        self.save_best_model = opt.save_best_model
+        self.default_root_dir = opt.default_root_dir
         self.conf = self.open_config()
-
+        self.neptune_logger= self.initialize_neptune()
 
 
 
@@ -46,16 +51,15 @@ class Train:
         with open(self.PATH_CONFIG) as f:
             conf = yaml.safe_load(f)
             conf["main_args"]={"exp_dir":self.save_best_model}
-            exp_dir = self.save_best_model
+            self.exp_dir = self.save_best_model
             return conf
 
     def initialize_neptune(self):
-        Epoch = self.conf["training"]["epochs"]
-
+        
         # Configurarmos el experimento y sus parametros
-        experiment_name = "Test_30_seconds_ConvTasnet_26.4_train_2.9_val_7.3_test_sta_"+str(start)+"_end_"+str(end)
+        experiment_name = self.experiment_name
         params=self.conf
-        tags = ["30_seconds_TEST_Best_Model_lote_start_"+str(start)+"_end_"+str(end)+"_ConvTasnet__train_from_PRETRAINED_best_epoch_"+str(best_epoch)]
+        tags = [self.tags]
 
         # Definir Logger 
         neptune_logger = NeptuneLogger(
@@ -63,32 +67,110 @@ class Train:
             project_name="josearangos/Tg-speech-separation",experiment_name=experiment_name,
             params = params, tags = tags, close_after_fit=False)
 
-
+        return neptune_logger
     
     def create_dataloaders(self):
-        pass
+        self.train_set = CallSpanish(
+                            csv_path=self.PATH_CSV_TRAIN,
+                            task="sep_clean",
+                            sample_rate=8000,
+                            n_src=2,
+                            segment=3
+                        )
+        self.val_set = CallSpanish(
+                        csv_path=self.PATH_CSV_VALID,
+                            task="sep_clean",
+                            sample_rate=8000,
+                            n_src=2,
+                            segment=3
+                        )
 
-        
+        self.train_loader = DataLoader(self.train_set,shuffle=True,batch_size=self.opt.batch_size, drop_last=True,num_workers=self.opt.num_workers)
+        self.val_loader = DataLoader(self.val_set, batch_size=self.opt.batch_size, drop_last=True,num_workers=self.opt.num_workers)
+            
+
+    def model_inicialize(self):
+        return BaseModel.from_pretrained("JorisCos/ConvTasNet_Libri2Mix_sepclean_8k").cuda()
+
+
+    def create_configure_model(self):
+        model = self.model_inicialize()
+        optimizer = make_optimizer(model.parameters(), **self.conf["optim"])
+
+        scheduler = None
+        if self.conf["training"]["half_lr"]:
+                scheduler = ReduceLROnPlateau(optimizer=optimizer, factor=0.5, patience=5)
+        gpus = -1 if torch.cuda.is_available() else None
+        distributed_backend = "ddp" if torch.cuda.is_available() else None
+
+        # Define Loss function.
+        loss_func = PITLossWrapper(pairwise_neg_sisdr, pit_from="pw_mtx")
+
+        self.system = System(
+                    model=model,
+                    loss_func=loss_func,
+                    optimizer=optimizer,
+                    train_loader=self.train_loader,
+                    val_loader=self.val_loader,
+                    scheduler=scheduler,
+                    config=self.conf,
+                )
+        # Define callbacks
+        callbacks = []
+        checkpoint_dir = os.path.join(self.exp_dir, "checkpoints/")
+        self.checkpoint = ModelCheckpoint(checkpoint_dir, monitor="val_loss", mode="min", save_top_k=5, verbose=True)
+
+        # Stop Early
+        callbacks.append(self.checkpoint)
+        if self.conf["training"]["early_stop"]:
+            callbacks.append(EarlyStopping(monitor="val_loss", mode="min", patience=30, verbose=True))
+
+        self.trainer= pl.Trainer(
+                max_epochs=self.conf["training"]["epochs"],
+                callbacks=callbacks,
+                default_root_dir=self.exp_dir,
+                gpus=gpus,
+                #distributed_backend=distributed_backend,
+                limit_train_batches=1.0,  # Useful for fast experiment
+                gradient_clip_val=5.0,
+                logger=self.neptune_logger
+                
+            )
+
+    def save_best_model(self):
+        best_k = {k: v.item() for k, v in self.checkpoint.best_k_models.items()}
+        with open(os.path.join(self.exp_dir, "best_k_models.json"), "w") as f:
+            json.dump(best_k, f, indent=0)
+
+        state_dict = torch.load(self.checkpoint.best_model_path)
+        self.system.load_state_dict(state_dict=state_dict["state_dict"])
+        self.system.cpu()
+
+        to_save = self.system.model.serialize()
+        best_model_path = os.path.join(self.exp_dir, "best_model.pth")
+        torch.save(to_save,best_model_path )
+
+        #Send best model to neptune
+        self.neptune_logger.experiment.log_artifact(best_model_path)
+                
     def run(self):
-        # Neptune
-
         #Create datasets train, valid, test
-
         #Create dataloaders
+        self.create_dataloaders()
 
-        # Create and configure model
+        # Create and configure model # Train model
+        self.trainer.fit(self.system)
+        
+        # Save model best model
+        self.save_best_model()
 
-        # Train model
 
-        # Save model
-
-        # End neptune
-
-        self.initialize_neptune()
-
+        self.neptune_logger.experiment.stop()
 
 
 
 
 if __name__ == '__main__':
-    Train().run()
+    # Read paramatres of command line
+    opt = BaseOptions().parse()
+    Train(opt).run()
