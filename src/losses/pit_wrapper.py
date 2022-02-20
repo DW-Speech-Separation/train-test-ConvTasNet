1b7df3cccc95041bab825f492ec4745858b82025
josearangos <jose.arangos@udea.edu.co>
2022-02-20 18:15:34 -0500

    pyannote version

--- src/losses/pit_wrapper.py
+++ src/losses/pit_wrapper.py
@@ -80,8 +80,8 @@ class PITLossWrapper(nn.Module):
             )
 
     def calculate_similarity(self,model, est_targets,num_layers):
-        waveforms_1 = torch.unsqueeze(est_targets[:,0],2)
-        waveforms_2 = torch.unsqueeze(est_targets[:,1],2)
+        waveforms_1 = torch.unsqueeze(est_targets[:,0],1)
+        waveforms_2 = torch.unsqueeze(est_targets[:,1],1)
 
         #(batch_size, num_channels, num_samples)
 
